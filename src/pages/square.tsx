bcfbc0902484f63feadc48a3de997fa008939116
valentinsejourne <v_sejourne@hetic.eu>
2023-03-23 13:02:58 +0100

    refactor: components

--- src/pages/square.tsx
+++ src/pages/square.tsx
@@ -1,3 +1,18 @@
-export default function Square({ value }: {value: string}) {
-    return <button className="square">{value}</button>;
+import { useState } from 'react';
+
+export  default function Square(): any {
+  const [value, setValue] = useState<any>(null);
+
+  function handleClick() {
+    setValue('X');
   }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
